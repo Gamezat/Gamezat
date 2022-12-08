b6595624bada0a716e3cf64f1997d119b2859d59
DuaaNawwas <duaa.nawwas@gmail.com>
2022-12-08 10:07:56 +0300

    modified users table

--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,15 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('image')->nullable();
+            $table->string('banner')->nullable();
+            $table->string('country')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->string('google_id')->nullable();
             $table->string('facebook_id')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('about')->nullable();
+            $table->string('games')->nullable();
             $table->string('role')->default('user');
             $table->softDeletes();
             $table->rememberToken();
