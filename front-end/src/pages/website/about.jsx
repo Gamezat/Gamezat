e0cbd961b3b42c49940f1d10d6f3d9d98276e424
emad-hash <emadhashash76@gmail.com>
2022-12-09 13:18:07 +0300

    About

---
+++ front-end/src/pages/website/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import About from './components/website/about/index'
+function about() {
+  return (
+    <div>	
+        <About/>	
+</div>
+  )
+}
+
+export default about
