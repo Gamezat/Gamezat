b07c71e645b549eb432e2834f7b7fb3b5a3b8cf8
AZZIE2000 <azzam.faraj0@gmail.com>
2022-12-11 23:45:57 +0300

    working on the reports section

--- front-end/src/pages/dashboard/DReports.jsx
+++ front-end/src/pages/dashboard/DReports.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import { HiSpeakerphone } from 'react-icons/hi';
 import RComments from '../../components/dashboard/reports/RComments';
 import Rgames from '../../components/dashboard/reports/Rgames';
 import Rpost from '../../components/dashboard/reports/Rpost';
 import Rreview from '../../components/dashboard/reports/Rreview';
 
+
 export default function DReports() {
+
+
+
+
     return (
 
         <>
-            <div className=' grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 lg:pl-[12%]  m-9 gap-6  mt-32 p-10 rounded-3xl border-t-4 shadow-lg border-grass  dark:bg-slate-800'>
-             
-                <RComments />
-                <Rpost />
-                <Rreview />
-                <Rgames />
+            <div className='  flex flex-wrap gap-10 m-9 p-10 rounded-3xl border-t-4 shadow-lg border-indigo  dark:bg-slate-800'>
+                <span className='flex items-center gap-5'>
+
+                    <HiSpeakerphone color='indigo' size={25} /> <p className='text-lg'>You can manage all the reports from here</p>
+                </span>
+                <div className=' w-full   flex gap-5 justify-center '>
+                    <div className='flex flex-col  flex-1 gap-5'>
+
+                        <RComments />
+                        <Rpost />
+                    </div>
+                    <div className='flex flex-col flex-1 gap-5'>
+
+                        <Rreview />
+                        <Rgames />
+                    </div>
+                </div>
 
 
             </div>
