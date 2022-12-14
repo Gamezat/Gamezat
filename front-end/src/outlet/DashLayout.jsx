ce39d54d9d10081f2287422611160cf492bdee56
DuaaNawwas <duaa.nawwas@gmail.com>
2022-12-14 18:17:42 +0300

    edit

--- front-end/src/outlet/DashLayout.jsx
+++ front-end/src/outlet/DashLayout.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useContext } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import SideBarDash from '../components/dashboard/SideBarDash';
-import { AuthContext } from '../context/AuthContext';
-
-
+import SideBarDash from "../components/dashboard/SideBarDash";
+import { AuthContext } from "../context/AuthContext";
 
 export default function UserEnd() {
-    const { cookies, user } = useContext(AuthContext)
-    const navigate = useNavigate()
+	const { cookies, user } = useContext(AuthContext);
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!cookies.Token && user?.role !== 'admin') {
-            // setShowPortal(true)
-            navigate("/");
-        }
-    }, [cookies.Token]);
-    return (
-        <>
-            <div className='flex'>
-
-                <SideBarDash />
-                <div className='flex-1 justify-center'>
-
-                    {/* An <Outlet> renders whatever child route is currently active in App.js */}
-                    <Outlet />
-                </div>
-            </div>
-
-
-        </>
-    )
+	useEffect(() => {
+		if (!cookies.Token) {
+			navigate("/");
+		}
+		if (cookies.Token && user?.role !== "admin") {
+			// setShowPortal(true)
+			navigate("/");
+		}
+	}, [cookies.Token]);
+	return (
+		<>
+			<div className="flex">
+				<SideBarDash />
+				<div className="flex-1 justify-center">
+					{/* An <Outlet> renders whatever child route is currently active in App.js */}
+					<Outlet />
+				</div>
+			</div>
+		</>
+	);
 }
